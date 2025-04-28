23b80ed4c827489e2f170fdf4ea895a6cb102465
AgungSaputra231230003 <231230003@unmuhpnk.ac.id>
2025-04-28 13:04:28 +0800

    git push origin main

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div>
+            <a href=""><button>Downloads</button></a>
+            <a href=""><button>Downloads</button></a>
+            </div>
+    </header>
+    <!--  -->
+    <main>
+
+    </main>
+    <!--  -->
+    <footer>
+
+    </footer>
+</body>
+</html>
